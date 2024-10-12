52b8ca35557ade063f14a202664dbdbc7c325067
ckt <3187572730@qq.com>
2024-10-12 10:24:22 +0800

    V 2.5 正式版

--- src/main/java/com/fhzn/bianpovisualization/controller/DeviceController.java
+++ src/main/java/com/fhzn/bianpovisualization/controller/DeviceController.java
@@ -48,40 +48,40 @@ public class DeviceController {
 
     final int maxRetries = 3; // 最大重试次数
 
-    // 接收前端请求，生成 token 并获取设备列表
-    @GetMapping("/device_serial_map")
-    public ResponseEntity<List<Map<String, Object>>> getDeviceSerialMap() {
-        int attempt=0;
-        while (attempt < maxRetries) {
-            try {
-                // 第一步：生成 token
-                String token = accessToken.getAccessToken(username, password);
-                System.out.println("获取token成功");
-
-                // 第二步：通过 token 获取设备列表
-                List<Map<String, Object>> deviceList = deviceListUtil.getDeviceList(token);
-
-                // 第三步：返回设备列表给前端
-                return new ResponseEntity<>(deviceList, HttpStatus.OK);
-            } catch (Exception e) {
-                attempt++;
-                e.printStackTrace();
-                System.out.println("获取设备列表失败，尝试次数: " + attempt);
-
-                // 如果达到最大重试次数，则抛出异常
-                if (attempt >= maxRetries) {
-                    throw new RuntimeException("获取设备列表失败，已达到最大重试次数");
-                }
-
-                try {
-                    Thread.sleep(1000); // 等待1秒后重试
-                } catch (InterruptedException ie) {
-                    Thread.currentThread().interrupt();
-                }
-            }
-        }
-        return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
-    }
+//    // 接收前端请求，生成 token 并获取设备列表
+//    @GetMapping("/device_serial_map")
+//    public ResponseEntity<List<Map<String, Object>>> getDeviceSerialMap() {
+//        int attempt=0;
+//        while (attempt < maxRetries) {
+//            try {
+//                // 第一步：生成 token
+//                String token = accessToken.getAccessToken(username, password);
+//                System.out.println("获取token成功");
+//
+//                // 第二步：通过 token 获取设备列表
+//                List<Map<String, Object>> deviceList = deviceListUtil.getDeviceList(token);
+//
+//                // 第三步：返回设备列表给前端
+//                return new ResponseEntity<>(deviceList, HttpStatus.OK);
+//            } catch (Exception e) {
+//                attempt++;
+//                e.printStackTrace();
+//                System.out.println("获取设备列表失败，尝试次数: " + attempt);
+//
+//                // 如果达到最大重试次数，则抛出异常
+//                if (attempt >= maxRetries) {
+//                    throw new RuntimeException("获取设备列表失败，已达到最大重试次数");
+//                }
+//
+//                try {
+//                    Thread.sleep(1000); // 等待1秒后重试
+//                } catch (InterruptedException ie) {
+//                    Thread.currentThread().interrupt();
+//                }
+//            }
+//        }
+//        return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+//    }
 
     @GetMapping("/device_map")
     public ResponseEntity<Map<String, Map<String, String>>> getDeviceMap() {
@@ -120,10 +120,10 @@ public class DeviceController {
         }
     }
 
-//    @EventListener(ApplicationReadyEvent.class)
-//    public void deviceCollection(){
-//        collectionService.startCollecting();
-//    }
+    @EventListener(ApplicationReadyEvent.class)
+    public void deviceCollection(){
+        collectionService.startCollecting();
+    }
 
 //    @PostMapping("/close")
 //    public void closeCollection(){
